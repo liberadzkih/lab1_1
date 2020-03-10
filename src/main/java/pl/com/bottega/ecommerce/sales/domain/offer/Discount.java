767dce86802a2a31d143d82a91d0fb22cb5f3866
debek.kacper <215702@edu.p.lodz.pl>
2020-03-10 18:12:46 +0100

    Added Discount class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+public class Discount {
+
+    private String cause;
+
+    private Money value;
+
+    public Discount(String cause, Money value) {
+        this.cause = cause;
+        this.value = value;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public Money getValue() {
+        return value;
+    }
+
+    public void setValue(Money value) {
+        this.value = value;
+    }
+}
