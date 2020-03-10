929bc55b47656fa29d2a440bb0c3c36d054b0f48
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-10 18:31:55 +0100

    Created class Product

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,39 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Date;
+
+public class Product {
+    private String id;
+    private String name;
+    private Date snapshotDate;
+    private String type;
+    private Money price;
+
+    public Product(String id, String name, Date snapshotDate, String type, Money price) {
+        this.id = id;
+        this.name = name;
+        this.snapshotDate = snapshotDate;
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
