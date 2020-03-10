e0d024dcefa91c671201523df30d479a8ddec3a3
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-03-10 18:38:18 +0100

    Updated class Discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -2,12 +2,18 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 
 public class Discount {
     private String cause;
+    private Money value;
 
-    public Discount(String cause) {
+    public Discount(String cause, Money value) {
         this.cause = cause;
+        this.value = value;
     }
 
     public String getCause() {
         return cause;
+    }
+
+    public Money getValue() {
+        return value;
     }
 }
