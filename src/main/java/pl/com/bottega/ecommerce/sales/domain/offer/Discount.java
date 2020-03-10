d74809a8898de7c517f768d5ef7ee276fcafd0e6
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-03-10 18:43:12 +0100

    Stworzenie klasy Discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,4 +1,25 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
+
 public class Discount {
+
+    private String discountCause;
+
+    private BigDecimal discount;
+
+    private String currency;
+
+    public Discount(String discountCause, BigDecimal discount) {
+        this.discountCause = discountCause;
+        this.discount = discount;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public String getDiscountCause() {
+        return discountCause;
+    }
 }
