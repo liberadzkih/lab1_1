3eb50497d1a489ca139aba4795ac8254db627049
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-03-11 08:14:09 +0100

    Discount, dodanie Money, hashcode, equals

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,25 +1,42 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Discount {
 
     private String discountCause;
 
-    private BigDecimal discount;
+    private Money discount;
 
-    private String currency;
-
-    public Discount(String discountCause, BigDecimal discount) {
+    public Discount(String discountCause, Money discount) {
         this.discountCause = discountCause;
         this.discount = discount;
     }
 
-    public BigDecimal getDiscount() {
+    public Money getDiscount() {
         return discount;
     }
 
     public String getDiscountCause() {
         return discountCause;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Discount discount1 = (Discount) o;
+        return Objects.equals(discountCause, discount1.discountCause) &&
+                Objects.equals(discount, discount1.discount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(discountCause, discount);
     }
 }
